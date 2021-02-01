6821cd5651273ab82e7e166722ffae86ddfdbfa2
F-Sukhy <nahar.rnkass@gmail.com>
2021-02-01 16:14:06 +0600

    switch

---
+++ castom.js
@@ -0,0 +1,95 @@
+    
+    
+    var marks = prompt("Enter your marks");
+        marks = parseInt(marks);
+
+    switch(marks){
+        case 80:
+        case 81:
+        case 82:
+        case 83:
+        case 84:
+        case 85:
+        case 86:
+        case 87:
+        case 88:
+        case 89:
+        case 90:
+        case 90:
+        case 91:
+        case 92:
+        case 93:
+        case 94:
+        case 95:
+        case 96:
+        case 97:
+        case 98:
+        case 99:
+        case 100:
+        
+            document.write("A+");
+            break;
+
+        case 79:
+        case 78:
+        case 77:
+        case 76:
+        case 75:
+        case 74:
+        case 73:
+        case 72:
+        case 71:
+        case 70:
+            document.write("A");
+        break;
+        case 69:
+        case 68:
+        case 67:
+        case 66:
+        case 65:
+        case 64:
+        case 63:
+        case 62:
+        case 61:
+        case 60:
+            document.write("A-");
+        break;
+        case 59:
+        case 58:
+        case 57:
+        case 56:
+        case 55:
+        case 54:
+        case 53:
+        case 52:
+        case 51:
+        case 50:
+            document.write("B");
+        break;
+        case 49:
+        case 48:
+        case 47:
+        case 46:
+        case 45:
+        case 44:
+        case 43:
+        case 42:
+        case 41:
+        case 40:
+            document.write("C");
+        break;
+        case 39:
+        case 38:
+        case 37:
+        case 36:
+        case 35:
+        case 34:
+        case 33:
+       
+            document.write("D");
+        break;
+
+
+        default:
+            document.write("F");
+    }
